b055be1e1c926b57af41b83279c59be50f4348e1
Dirk Kok <the@dirkkok.nl>
2021-06-19 19:34:17 +0200

    Initial commit (1.17)

---
+++ README.md
@@ -0,0 +1,20 @@
+## Vulpine Retribution
+Are your friends killing foxes? Installing this mod is a simple solution.
+
+No amount of armor, quantum suits, totems of undying, creative mode or straight up cheating will let them get away with it.
+
+If you install this mod on a server, then clients do not need the mod nor Fabric installed for it to work. If there are no other mods, or the other mods permit it, then clients can be vanilla.
+
+Built for Minecraft 1.17 with Fabric. Should probably work on most Fabric versions. Feel free to ask for a backport.
+### License
+This mod is licensed AGPL 3.0. The short version is that if you distribute a fork of this mod, you must also distribute the source code.
+Notably, under Affero, hosting a server with this mod installed constitutes distribution.
+
+### Modpack permission
+Granted.
+
+### Disclaimer
+No foxes were harmed in the making of this mod. I did kill several chickens and a sheep. See VulpineRetribution.java, line 16 for more details.
+
+The mod icon is a composite of [this icon](https://svgsilh.com/image/305229.html) which is public domain,
+and [this icon](https://thenounproject.com/term/fox-tail/135949/) by Lucid Formation, licensed CC-BY.
---
+++ src/main/java/nl/dirkkok/minecraft/fabric/vulpineretribution/VulpineRetribution.java
@@ -0,0 +1,41 @@
+package nl.dirkkok.minecraft.fabric.vulpineretribution;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
+
+public class VulpineRetribution implements ModInitializer {
+	private static final boolean DEBUG = true;
+
+	public EntityType<?> getTargetEntityType() {
+		// In order to test this mod properly I would have to kill a fox. So I just assumed that it works properly based on the fact it works in dev.
+		// Snowy foxes and red foxes are the same entity, with a different Type (enum, see source code) so no need to include something like SnowyFoxEntity.
+		if (DEBUG) {
+			return EntityType.CHICKEN;
+		} else {
+			return EntityType.FOX;
+		}
+	}
+
+	@Override
+	public void onInitialize() {
+		ServerEntityCombatEvents.AFTER_KILLED_OTHER_ENTITY.register((ServerWorld world, Entity entity, LivingEntity killedEntity) -> {
+			if (entity instanceof PlayerEntity && killedEntity.getType() == getTargetEntityType()) {
+				smiteEntity(entity);
+			}
+		});
+	}
+
+	private void smiteEntity(Entity target) {
+		LightningEntity bolt = new LightningEntity(EntityType.LIGHTNING_BOLT, target.getEntityWorld());
+		bolt.setCosmetic(true); // Because we explicitly kill the player (regardless of current health or armor)
+		bolt.setPos(target.getX(), target.getY(), target.getZ());
+		target.getCommandSource().getWorld().spawnEntity(bolt);
+		target.kill();
+	}
+}
